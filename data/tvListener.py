8e35bc0c27e93128e5e94a85da0ebe9f082f14ae
Taylor Lloyd <taylor@taylorlloyd.ca>
2017-01-07 10:27:01 -0700

    Jekyll Build

---
+++ data/tvListener.py
@@ -0,0 +1,168 @@
+'''
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+ '''
+
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import sys
+import logging
+import time
+import json
+import getopt
+import subprocess
+
+def turnOff():
+  return_code = subprocess.call("irsend send_once lg POWER_OFF", shell=True)
+  if return_code != 0:
+    print("Error turning off TV: "+ str(return_code))
+  return return_code
+
+def setInput(input):
+  return_code = subprocess.call("irsend send_once lg " + input + "; irsend send_once lg POWER_ON; sleep 11; irsend send_once lg " + input, shell=True)
+  if return_code != 0:
+    print("Error setting TV to "+input+": "+ str(return_code))
+  return return_code
+
+# Custom Shadow callback
+def customShadowCallback_Delta(payload, responseStatus, token):
+	# payload is a JSON string ready to be parsed using json.loads(...)
+	# in both Py2.x and Py3.x
+	print(responseStatus)
+	payloadDict = json.loads(payload)
+	print("++++++++DELTA++++++++++")
+        if "power" in payloadDict["state"]:
+	  print("power: " + str(payloadDict["state"]["power"]))
+        if "input" in payloadDict["state"]:
+	  print("input: " + str(payloadDict["state"]["input"]))
+	print("version: " + str(payloadDict["version"]))
+	print("+++++++++++++++++++++++\n\n")
+
+	if not payloadDict["state"]["power"]:
+	  turnOff()
+        else:
+          setInput(payloadDict["state"]["input"])
+
+# Usage
+usageInfo = """Usage:
+
+Use certificate based mutual authentication:
+python basicShadowDeltaListener.py -e <endpoint> -r <rootCAFilePath> -c <certFilePath> -k <privateKeyFilePath>
+
+Use MQTT over WebSocket:
+python basicShadowDeltaListener.py -e <endpoint> -r <rootCAFilePath> -w
+
+Type "python basicShadowDeltaListener.py -h" for available options.
+
+
+"""
+# Help info
+helpInfo = """-e, --endpoint
+	Your AWS IoT custom endpoint
+-r, --rootCA
+	Root CA file path
+-c, --cert
+	Certificate file path
+-k, --key
+	Private key file path
+-w, --websocket
+	Use MQTT over WebSocket
+-h, --help
+	Help information
+
+
+"""
+
+# Read in command-line parameters
+useWebsocket = False
+host = ""
+rootCAPath = ""
+certificatePath = ""
+privateKeyPath = ""
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "hwe:k:c:r:", ["help", "endpoint=", "key=","cert=","rootCA=", "websocket"])
+	if len(opts) == 0:
+		raise getopt.GetoptError("No input parameters!")
+	for opt, arg in opts:
+		if opt in ("-h", "--help"):
+			print(helpInfo)
+			exit(0)
+		if opt in ("-e", "--endpoint"):
+			host = arg
+		if opt in ("-r", "--rootCA"):
+			rootCAPath = arg
+		if opt in ("-c", "--cert"):
+			certificatePath = arg
+		if opt in ("-k", "--key"):
+			privateKeyPath = arg
+		if opt in ("-w", "--websocket"):
+			useWebsocket = True
+except getopt.GetoptError:
+	print(usageInfo)
+	exit(1)
+
+# Missing configuration notification
+missingConfiguration = False
+if not host:
+	print("Missing '-e' or '--endpoint'")
+	missingConfiguration = True
+if not rootCAPath:
+	print("Missing '-r' or '--rootCA'")
+	missingConfiguration = True
+if not useWebsocket:
+	if not certificatePath:
+		print("Missing '-c' or '--cert'")
+		missingConfiguration = True
+	if not privateKeyPath:
+		print("Missing '-k' or '--key'")
+		missingConfiguration = True
+if missingConfiguration:
+	exit(2)
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTShadowClient
+myAWSIoTMQTTShadowClient = None
+if useWebsocket:
+	myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient("basicShadowDeltaListener", useWebsocket=True)
+	myAWSIoTMQTTShadowClient.configureEndpoint(host, 443)
+	myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath)
+else:
+	myAWSIoTMQTTShadowClient = AWSIoTMQTTShadowClient("basicShadowDeltaListener")
+	myAWSIoTMQTTShadowClient.configureEndpoint(host, 8883)
+	myAWSIoTMQTTShadowClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTShadowClient configuration
+myAWSIoTMQTTShadowClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTShadowClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTShadowClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect to AWS IoT
+myAWSIoTMQTTShadowClient.connect()
+
+# Create a deviceShadow with persistent subscription
+TV = myAWSIoTMQTTShadowClient.createShadowHandlerWithName("TV", True)
+
+# Listen on deltas
+TV.shadowRegisterDeltaCallback(customShadowCallback_Delta)
+
+# Loop forever
+while True:
+	pass
